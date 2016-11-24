492c4ea2b692a9804f31d409a2cf4a396624dc8a
martingraham <martinjamesgraham@outlook.com>
2016-11-24 16:16:15 +0000

    password restore completed

--- php/passwordUpdate.php
+++ php/passwordUpdate.php
@@ -9,8 +9,8 @@ try {
     if (isset($_POST['token'])){
         $ptoken=$_POST['token'];
     }
-    if (!$ptoken){
-      echo (json_encode(array ("status"=>"fail", "msg"=> "No token received.")));
+    if (!$ptoken || !filter_var ($ptoken, FILTER_VALIDATE_REGEXP, array ('options' => array ('regexp' => '/.{28,}/')))) {
+      echo (json_encode(array ("status"=>"fail", "msg"=> "No valid token received.")));
       exit;
     }
     
@@ -31,22 +31,21 @@ try {
         /* test if token exists */
         pg_prepare ($dbconn, "doesTokenExist", "SELECT * FROM users WHERE ptoken = $1 AND ptoken <> ''");
         $result = pg_execute($dbconn, "doesTokenExist", [$ptoken]);
-        $returnRow = pg_fetch_assoc ($result);
-        
         $count=intval(pg_numrows($result));
-
-        error_log (print_r ($returnRow, true));    
         
         if ($count == 1) {
             $returnRow = pg_fetch_assoc ($result);
             $id = $returnRow['id'];
+            error_log (print_r ($returnRow, true));
             $hash = password_hash ($pword, PASSWORD_BCRYPT);
-            pg_prepare ($dbconn, "setNewPassword", "UPDATE users SET password = $2, ptoken = '' FROM users WHERE id = $1");
+            pg_prepare ($dbconn, "setNewPassword", "UPDATE users SET password = $2, ptoken = '' WHERE id = $1");
             $result = pg_execute($dbconn, "setNewPassword", [$id, $hash]);
+            
             pg_query("COMMIT");
             $json = json_encode(array ("status"=>"success", "msg"=> "Password reset complete. Thankyou."));
             echo ($json);
         } else {
+            pg_query("COMMIT");
             $json = json_encode(array ("status"=>"fail", "msg"=> "Token not matched to any user. Password update failed."));
             echo ($json);
         }
