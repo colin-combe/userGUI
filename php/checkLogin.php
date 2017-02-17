9dc9d1617458c82e964b0a2b30d0b2a7429aeb89
Martin Graham <martinjamesgraham@outlook.com>
2017-02-17 11:48:14 +0000

    login scripts moved into userGUI project

---
+++ php/checkLogin.php
@@ -0,0 +1,54 @@
+<?php
+include('../../connectionString.php');
+// Connect to server
+$dbconn = pg_connect($connectionString)or die("cannot connect");
+$date = date("d-M-Y H:i:s");
+
+try {
+    pg_query("BEGIN") or die("Could not start transaction\n");
+
+    error_log (print_r ($_POST, true));
+    // Define $myusername and $mypass
+    $myusername=$_POST['login-name'];
+    $mypass=$_POST['login-pass'];
+
+    // To protect MySQL injection
+    $myusername = stripslashes($myusername);
+    $mypass = stripslashes($mypass);
+
+    $get = pg_prepare($dbconn, "getUserData", "SELECT password, id FROM users WHERE user_name=$1");
+    $result = pg_execute($dbconn, "getUserData", [$myusername]);
+    $line = pg_fetch_assoc($result);
+    $hash = $line["password"];
+    $user_id = $line["id"];
+
+    // Mysql_num_row is counting table row
+    $count = pg_numrows($result);
+    $date = date("d-M-Y H:i:s");
+    
+    // if no username match
+    if ($count === 0) { 
+        echo (json_encode(array ("status"=>"fail", "msg"=>"< Incorrect Password / Username combination", "field"=>"login-name")));
+    }
+    // If result matched $myusername and $mypass, table row must be 1 row
+    else if ($count === 1 && password_verify ($mypass, $hash)) {
+        session_start();
+        $_SESSION['session_name'] = $myusername;
+        $_SESSION['user_id'] = $user_id;
+        //$redirectTo = empty($_POST["redirect"]) ? "../history/history.html" : $_POST["redirect"];
+        $redirectTo = "../history/history.html";
+        error_log (print_r ($redirectTo, true));
+        echo (json_encode(array ("status"=>"success", "redirect"=> $redirectTo)));
+    } else {
+        echo (json_encode(array ("status"=>"fail", "msg"=>"< Incorrect Password / Username combination", "field"=>"login-name")));
+    }
+    pg_query("COMMIT");
+} catch (Exception $e) {
+    pg_query("ROLLBACK");
+    $msg = ($e->getMessage()) ? ($e->getMessage()) : "An error occurred when attempting to log in.";
+    echo (json_encode(array ("status"=>"fail", "error"=> $msg."<br>".$date)));
+}
+
+//close connection
+pg_close($dbconn);
+?>
---
+++ php/logout.php
@@ -0,0 +1,25 @@
+<?php
+ob_start();
+// http://stackoverflow.com/questions/18188716/warning-session-destroy-trying-to-destroy-uninitialized-session
+session_start(); 
+// http://stackoverflow.com/questions/3512507/proper-way-to-logout-from-a-session-in-php
+
+// Unset all of the session variables.
+$_SESSION = array();
+
+// If it's desired to kill the session, also delete the session cookie.
+// Note: This will destroy the session, and not just the session data!
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
+
+// Finally, destroy the session.
+session_destroy();
+
+header("location:../userLogin.html");
+ob_end_flush();
+?>
