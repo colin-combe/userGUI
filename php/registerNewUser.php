f1c7d5d0d47325083b8ec04bce71e20a41a414d5
Colin Combe <colin.combe@ed.ac.uk>
2017-11-28 16:34:38 +0000

    PDO used in registerNewUser

--- php/registerNewUser.php
+++ php/registerNewUser.php
@@ -1,5 +1,5 @@
 <?php
-include ('../../connectionString.php');
+include ('../../xiUI-connectionString.php');
 include ('../../../xi_ini/emailInfo.php');
 include ('utils.php');
 
@@ -16,21 +16,41 @@ try {
     // validate captcha
     validateCaptcha ($captcha);
         
-    $dbconn = pg_connect($connectionString);
+    //~ $dbconn = pg_connect($connectionString);
+    $charset = 'utf8mb4';
+    $dsn = "mysql:host=$server;dbname=$db;charset=$charset";
+    $opt = [
+        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES   => false,
+    ];
+    $pdo = new PDO($dsn, $user, $password, $opt);
+    
     try {
         /* test if username already exists */
-        pg_prepare ($dbconn, "doesUserExist", "SELECT COUNT(user_name) FROM users WHERE user_name = $1;");
-        $result = pg_execute($dbconn, "doesUserExist", [$username]);
-        $returnRow = pg_fetch_assoc ($result);
+        //pg_prepare ($dbconn, "doesUserExist", "SELECT COUNT(user_name) FROM users WHERE user_name = $1;");
+        $stmt = $pdo->prepare('SELECT COUNT(user_name) AS count FROM users WHERE user_name = ?');
+
+        //$result = pg_execute($dbconn, "doesUserExist", [$username]);
+        $stmt->execute([$username]);
+        
+        //$returnRow = pg_fetch_assoc ($result);
+        $returnRow = $stmt->fetch();
+
         if (intval($returnRow["count"]) > 0) {
             echo (json_encode(array ("status"=>"fail", "field"=> "username", "msg"=>getTextString("newUserUniqueNameError", [$username]), "revalidate"=> true)));
             exit;
         }
         
         /* test if email already exists */
-        pg_prepare ($dbconn, "doesEmailExist", "SELECT COUNT(email) FROM users WHERE email = $1;");
-        $result = pg_execute($dbconn, "doesEmailExist", [$email]);
-        $returnRow = pg_fetch_assoc ($result);
+        //pg_prepare ($dbconn, "doesEmailExist", "SELECT COUNT(email) FROM users WHERE email = $1;");
+        $stmt = $pdo->prepare("SELECT COUNT(email) AS count FROM users WHERE email = ?");
+        
+        //$result = pg_execute($dbconn, "doesEmailExist", [$email]);
+        $stmt->execute([$email]);
+        
+        //$returnRow = pg_fetch_assoc ($result);
+        $returnRow = $stmt->fetch(); 
         if (intval($returnRow["count"]) > 0) {
             echo (json_encode(array ("status"=>"fail", "field"=> "email", "msg"=>getTextString("newUserUniqueEmailError", [$email]), "revalidate"=> true)));
             exit;
@@ -38,17 +58,30 @@ try {
 
         // database has 20 character limit on user_name field, throws sql error if bigger
         // limit use of existing user_name to generate new name, as if it's 20 chars the new name would be identical and this will throw an error too
-        pg_query("BEGIN") or die("Could not start transaction\n");
+        
+        //pg_query("BEGIN") or die("Could not start transaction\n");
+        $pdo->beginTransaction();
+
         $hash = $pword ? password_hash ($pword, PASSWORD_BCRYPT) : "";
-        $newUser = pg_prepare($dbconn, "newUser", "INSERT INTO users (user_name, password, email, max_aas, max_spectra) VALUES($1, $3, $2, 100000000, 100000) RETURNING id, user_name");
-        $result = pg_execute($dbconn, "newUser", [$username, $email, $hash]);
-        $returnRow = pg_fetch_assoc ($result); // return the inserted row (or selected parts thereof)
-        $returnedID = $returnRow["id"];
 
-        $addUserToGroup = pg_prepare($dbconn, "addUserToGroup", "INSERT INTO user_in_group (user_id, group_id) VALUES($1, $2)");
+        //$newUser = pg_prepare($dbconn, "newUser", "INSERT INTO users (user_name, password, email, max_aas, max_spectra) VALUES($1, $3, $2, 100000000, 100000) RETURNING id, user_name");
+        $newUser = $pdo->prepare("INSERT INTO users (user_name, password, email) VALUES(?, ?, ?)");
+        
+        //$result = pg_execute($dbconn, "newUser", [$username, $email, $hash]);
+        $newUser->execute([$username, $hash, $email]);
+        
+        //$returnRow = pg_fetch_assoc ($result); // return the inserted row (or selected parts thereof)
+        $returnRow = $newUser->fetch();  
+        //~ $returnedID = $returnRow["id"];
+        
+		//following will need to be reinstated for 'xi installation'
+/*
+        $addUserToGroup = pg_prepare($dbconn, "addUserToGroup", "INSERT INTO user_in_group (user_id, group_id) VALUES($1, $2)"); 
         $result = pg_execute($dbconn, "addUserToGroup", [$returnedID, "12"]);
+*/
 
-         pg_query("COMMIT");
+        //pg_query("COMMIT");
+        $pdo->commit();
 
         require_once    ('../vendor/php/PHPMailer-master/class.phpmailer.php');
         require_once    ('../vendor/php/PHPMailer-master/class.smtp.php');
@@ -65,14 +98,19 @@ try {
             echo ($json);
         }
     } catch (Exception $e) {
-         pg_query("ROLLBACK");
+         //pg_query("ROLLBACK");
+         $pdo->rollback();
+         
          $date = date("d-M-Y H:i:s");
          $msg = ($e->getMessage()) ? ($e->getMessage()) : getTextString("newUserErrorCatchall");
          echo (json_encode(array ("status"=>"fail", "msg"=> $msg."<br>".$date, "revalidate"=> true)));
+         
+         throw $e;
     }
 
     //close connection
-    pg_close($dbconn);
+    //pg_close($dbconn);
+    $pdo = null;
 
 } catch (Exception $e) {
      $msg = ($e->getMessage()) ? ($e->getMessage()) : getTextString("newUserErrorCatchall");
--- userReg.php
+++ userReg.php
@@ -13,104 +13,17 @@
         <script src="vendor/js/jquery-3.2.1.js"></script>
         <script src="vendor/js/hideShowPassword.js"></script>
         <script src="vendor/js/spin.js"></script>
-         <script src="./js/forms.js"></script>
+        <script src="./js/forms.js"></script>
      </head>
-    
+
      <body>
-         
+
         <header>
             <span class="topRight"><button id="helpButton"></button></span>
             <h1>Create New Xi User Account</h1>
         </header>
 
-        <div class="login">
-
-              <form id="register_form" name="register_form" method="post" action="./php/registerNewUser.php" class="login-form">
-                  
-                <div class="control-group">
-                    <label for="email">Pick Username</label>
-                    <input type="text" value="" placeholder="Username" id="username" name="username" pattern="^[a-zA-Z0-9-_.]{4,16}" oninput="this.setCustomValidity('')" required autofocus/>
-                    <span class="error" id="username-errorMsg"></span>
-                    <span class="error2"></span>
-                </div>
-                  
-                <div class="control-group">
-                    <label for="email">Email Address</label>
-                    <input type="email" value="" placeholder="email@address" id="email" name="email" pattern="" required/>
-                    <span class="error" id="email-errorMsg"></span>
-                    <span class="error2"></span>
-                </div>
-
-                <div class="control-group">
-                    <label for="pass1">Pick Password</label>
-                    <input type="password" value="" placeholder="Password" id="pass" name="pass" pattern=".{6,}" oninput="this.setCustomValidity('')" required/>
-                    <span class="error" id="pass-errorMsg"></span>
-                    <input type="checkbox" id="show-password">
-                    <label for="show-password">Show password</label>
-                </div>
-                  
-                <div id="recaptchaWidget" data-sitekey="getFromConfig.json"></div>
-                <span class="error">&lt; Please check the captcha form</span>
-                <br/>
-                  
-                <input name="Submit" value="Create My Xi Account" type="submit" class="btn btn-1a"/>
-
-              </form>
-            
-            <div id="spinBox"></div>
-            <div id="msg"></div>
-
-            <script type="text/javascript"> 
-                //$(document).ready(function(e) {
-                var onloadCallback = function () {
-                    $.when (
-                        $.getJSON("./json/config.json"),
-                        $.getJSON("./json/msgs.json")
-                    ).done (function (configxhr, msgsxhr) {
-                        var config = configxhr[0];
-                        var msgs = msgsxhr[0];
-                        CLMSUI.loginForms.msgs = msgs;
-                        CLMSUI.loginForms.makeFooter();
-                        CLMSUI.loginForms.makeHelpButton();
-                        CLMSUI.loginForms.finaliseRecaptcha (config.googleRecaptchaPublicKey);
-                        var spinner = CLMSUI.loginForms.getSpinner();
-
-                        var splitRegex = config.emailRegex.split("/");
-                        $("#email").attr("pattern", splitRegex[1]);
-
-                        var nameValidationMsg = CLMSUI.loginForms.getMsg("clientNameValidationInfo");
-                        $("#username").attr("oninvalid", "this.setCustomValidity('"+nameValidationMsg+"')");
-                        $("#username-errorMsg").text("< "+nameValidationMsg);
-
-                        var passwordValidationMsg = CLMSUI.loginForms.getMsg("clientPasswordValidationInfo");
-                        $("#pass").attr("oninvalid", "this.setCustomValidity('"+passwordValidationMsg+"')");
-                        $("#pass-errorMsg").text("< "+passwordValidationMsg);
-
-                        var emailValidationMsg = CLMSUI.loginForms.getMsg("clientEmailValidationInfo");
-                        $("#email-errorMsg").text("< "+emailValidationMsg);
-
-                        // Example 3:
-                        // - When checkbox changes, toggle password
-                        //   visibility based on its 'checked' property
-                        $('#show-password').change(function(){
-                          $('#pass').hideShowPassword($(this).prop('checked'));
-                        });
-
-
-                        // divert form submit action to this javascript function
-                        $("#register_form").submit (function(e) {
-                            $(".error2").css("display", "none");
-                            spinner.spin (document.getElementById ("spinBox"));
-                            CLMSUI.loginForms.ajaxPost (e.target, {"g-recaptcha-response": grecaptcha.getResponse()}, function() { spinner.stop(); });
-                            e.preventDefault();
-                        });
-                    });
-                }
-                //});
-            </script>
-            
-            <script src="https://www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit" async defer></script>
-        </div>
+        <?php include ("userRegInnerDiv.php")?>
 
     </body>
 </html>
