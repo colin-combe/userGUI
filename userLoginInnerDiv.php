66c1a9c51f19e25470ad01f31143f46f40ad2e66
Colin Combe <colin.combe@ed.ac.uk>
2017-12-05 16:06:37 +0000

    split userLogin.html into two php files

--- userLoginInnerDiv.php
+++ userLoginInnerDiv.php
@@ -1,50 +1,3 @@
-<!DOCTYPE html>
-
-<!--
-//  CLMS-UI
-//  Copyright 2015 Colin Combe, Rappsilber Laboratory, Edinburgh University
-//
-//  This file is part of CLMS-UI.
-//
-//  CLMS-UI is free software: you can redistribute it and/or modify
-//  it under the terms of the GNU General Public License as published by
-//  the Free Software Foundation, either version 3 of the License, or
-//  (at your option) any later version.
-//
-//  CLMS-UI is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU General Public License for more details.
-//
-//  You should have received a copy of the GNU General Public License
-//  along with CLMS-UI.  If not, see <http://www.gnu.org/licenses/>.
--->
-
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <title>Xi User Login</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <meta name="description" content="a common platform for the downstream analysis of CLMS data" />
-      <meta name="google" content="notranslate" />
-      <meta name="author" content="Rappsilber Laboratory">
-      <meta name="copyright" content="2016">
-      <meta charset="UTF-8">
-
-      <link rel="stylesheet" href="../xi3/css/common.css" />
-      <link rel="stylesheet" href="css/login.css" />
-      
-      <script type="text/javascript" src="./vendor/js/zepto.js"></script>
-      <script type="text/javascript" src="./js/forms.js"></script>
-  </head>
-    
-  <body>
-
-	<header>
-        <span class="topRight"><button id="helpButton"></button></span>
-        <h1>Xi User Login</h1>
-	</header>
-
     <div class="login">
 
           <form id="login_form" name="login_form" method="post" action="./php/checkLogin.php" class="login-form">
@@ -119,6 +72,3 @@
             });
         </script>
     </div>
-
-  </body>
-</html>
